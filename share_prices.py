a17b21c6f97084155016be50d2c298a7f8e511b7
Robert Rees <shudderfix@gmail.com>
2016-05-12 08:46:38 +0100

    Reset tracking after the discovery of a local minima

--- share_prices.py
+++ share_prices.py
@@ -10,17 +10,21 @@ def calculate_margin(prices):
 			min = price
 			continue
 
-		if not max:
-			if price > min:
-				max = price
-				continue
-
 		if price < min:
 			min = price
+
+		if not max:
+			max = price
+			continue
 
 		if price > max:
 			max = price
 
-		best_margin = max - min
+		margin = max - min
+
+		if not best_margin or margin > best_margin:
+			best_margin = margin
+			min = None
+			max = None
 
 	return best_margin
--- test_share_prices.py
+++ test_share_prices.py
@@ -9,3 +9,10 @@ class TestMargins(unittest.TestCase):
 		expected_margin = 2
 
 		self.assertEqual(share_prices.calculate_margin(prices), expected_margin)
+
+	def test_second_interval(self):
+
+		prices = [3, 3, 1, 2, 5, 3, 2, 7, 12]
+		expected_margin = 10
+
+		self.assertEqual(share_prices.calculate_margin(prices), expected_margin)
