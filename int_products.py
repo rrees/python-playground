e6dad7077053e55595633c5405c3ef695fa0184d
Robert Rees <shudderfix@gmail.com>
2016-05-14 08:33:49 +0100

    Version of calculation in O(2n -1)?

--- int_products.py
+++ int_products.py
@@ -2,6 +2,46 @@ import functools
 
 def calculate(ints):
 
+
+	left = 1
+	rights = [1]
+
+	for i in range(len(ints) - 1, 0, -1):
+		rights = [ints[i] * rights[0]] + rights
+
+	totals = []
+	for i in ints:
+		totals.append(left * rights[0])
+
+		left = left * i
+		rights = rights[1:]
+
+	return totals
+
+def calculate_brute_force(ints):
+
+	result = []
+
+	for i in range(0, len(ints)):
+		total = None
+
+		for j in range(0, len(ints)):
+			if i == j:
+				continue
+
+			if total == None:
+				total = ints[j]
+			else:
+				total = total * ints[j]
+
+		result.append(total)
+
+
+	return result
+
+
+def calculate_lists(ints):
+
 	def product(ints):
 		return functools.reduce(lambda acc, val: acc * val, ints)
 
