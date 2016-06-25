c66f37552e1dad10ae99ab525ec91aeea6565931
Robert Rees <shudderfix@gmail.com>
2016-06-25 13:08:58 +0100

    Working Heap permutation implementation

--- heap_permutations.py
+++ heap_permutations.py
@@ -1,3 +1,4 @@
+import copy
 
 def swap(i, j, array):
 	item = array[i]
@@ -6,7 +7,6 @@ def swap(i, j, array):
 	return array
 
 def permutations(array, n = None, results = None):
-	print(array, n, results)
 
 	if not n:
 		n = len(array)
@@ -15,16 +15,16 @@ def permutations(array, n = None, results = None):
 		results = []
 
 	if n == 1:
-		print(array, results)
-		results.append(array.copy())
-		print(array, results)
+		results.append(copy.deepcopy(array))
 		return results
 
-	for i in range(n):
+	for i in range(n - 1):
 		results = permutations(array, n - 1, results)
 		if n % 2 == 0:
-			swap(i, -1, array)
+			swap(i, n - 1, array)
 		else:
-			swap(0, -1, array)
+			swap(0, n - 1, array)
+
+	results = permutations(array, n - 1, results)
 
 	return results
--- test_permutations.py
+++ test_permutations.py
@@ -12,3 +12,10 @@ class TestPermutations(unittest.TestCase):
 		permutations = [[1,2], [2,1]]
 
 		self.assertEqual(hp.permutations(array), permutations)
+
+	def test_complex_permutations(self):
+		array = [1, 2, 3]
+
+		permutations = [[1,2,3], [2,1,3], [3,1,2], [1,3,2], [3,2,1], [2,3,1]]
+
+		self.assertCountEqual(hp.permutations(array), permutations)
