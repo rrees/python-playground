d18f87647e825c0df66c27018a0cc0224a84d03b
Robert Rees <shudderfix@gmail.com>
2016-07-03 17:54:02 +0100

    Uses a named tuple to pass the recursion data around

--- graphs.py
+++ graphs.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+
+SearchData = namedtuple('SearchData', ['path_weights', 'previous', 'remaining'])
+
 def recreate_path(i, j, previous):
 	path = []
 	path_node = j
@@ -6,35 +10,37 @@ def recreate_path(i, j, previous):
 		path_node = previous[path_node]
 	return [i] + path
 
-def shortest_route(i, j, n, edges, path_weights=None, previous=None, remaining=None):
-	if not remaining == None and len(remaining) == 0:
-		return (recreate_path(i,j, previous), path_weights[j])
+def shortest_route(i, j, n, edges, search_data=None):
 
-	if path_weights == None:
+	if search_data == None:
 		initial_weight = 1 + sum(edges.values())
-		path_weights = {p:0 if p == i else initial_weight for p in range(n)}
+		path_weights =  {p:0 if p == i else initial_weight for p in range(n)}
 
-	if remaining == None:
-		remaining = [p for p in range(n)]
-		remaining = sorted(remaining, key = lambda x: path_weights[x])
+		search_data = SearchData(
+			path_weights = path_weights,
+			previous = {},
+			remaining = sorted([p for p in range(n)], key = lambda x: path_weights[x])
+			)
 
-	if previous == None:
-		previous = {}	
+	if search_data and len(search_data.remaining) == 0:
+		return (recreate_path(i,j, search_data.previous), search_data.path_weights[j])
 
-	current_node = remaining[0]
+	current_node = search_data.remaining[0]
 
 	adjacent_nodes = [b for a, b in edges.keys() if a == current_node]
 		
 	#print(adjacent_nodes)
 
 	for node in adjacent_nodes:
-		next_step = path_weights[current_node] + edges[(current_node, node)]
-		if path_weights[node] > next_step:
-			previous[node] = current_node
-			path_weights[node] = next_step
+		next_step = search_data.path_weights[current_node] + edges[(current_node, node)]
+		if search_data.path_weights[node] > next_step:
+			search_data.previous[node] = current_node
+			search_data.path_weights[node] = next_step
 
-	remaining = remaining[1:]
-
-	return shortest_route(i, j, n, edges, path_weights, previous, remaining)
+	return shortest_route(i, j, n, edges, search_data = SearchData(
+		path_weights = search_data.path_weights,
+		previous = search_data.previous,
+		remaining = search_data.remaining[1:]
+		))
 
 
