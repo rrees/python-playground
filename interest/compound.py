1a327cb2c70604293a58a674b93054370e6a314e
Robert Rees <shudderfix@gmail.com>
2024-05-03 22:59:27 +0100

    Combines the two projects to generate a comparision

--- interest/compound.py
+++ interest/compound.py
@@ -33,15 +33,19 @@ print(
     f"Principle: {principle}; base daily interest {daily_interest:.2f}, adjusted daily interest {adjusted_daily_interest:.2f}"
 )
 
-base_projection = interest_projection(principle, interest_rate)
-adjusted_projection = interest_projection(principle + adjustment, interest_rate)
 
-days = 10
+def display_projection(principle, interest_rate, adjustment, days=30):
+    base_projection = interest_projection(principle, interest_rate)
+    adjusted_projection = interest_projection(principle + adjustment, interest_rate)
 
-for current_balance, daily_interest in islice(base_projection, 10):
-    print(f"Balance: {current_balance:.2f}, daily interest {daily_interest:.2f}")
+    for [[base_balance, base_interest], [adjusted_balance, adjusted_interest]] in zip(
+        islice(base_projection, days), islice(adjusted_projection, days)
+    ):
+        balance_difference = adjusted_balance - base_balance - adjustment
+        interest_difference = adjusted_interest - base_interest
+        print(
+            f"Balance: {adjusted_balance:.2f} ({balance_difference:.2f}), daily interest {adjusted_interest:.2f} ({interest_difference:.2f})"
+        )
 
-print()
 
-for current_balance, daily_interest in islice(adjusted_projection, 10):
-    print(f"Balance: {current_balance:.2f}, daily interest {daily_interest:.2f}")
+display_projection(principle, interest_rate, adjustment)
