4043b86564bf9a6348dd90fceb199e7a079a3ca0
Robert Rees <shudderfix@gmail.com>
2025-01-29 10:51:27 +0000

    Do not re-write the output file if it already exists

--- rss_reader/reader.py
+++ rss_reader/reader.py
@@ -5,29 +5,37 @@ from content.convert_page import convert_content
 from feeds.rss import load_feed
 from urls import is_valid_url
 
+DEFAULT_URLS = [
+    "https://www.goonhammer.com/",
+]
+
+urls_to_read = []
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python reader.py <url to read>")
-        exit(1)
+        urls_to_read + DEFAULT_URLS
+    else:
+        url = sys.argv[1]
 
-    url = sys.argv[1]
+        if not is_valid_url(url):
+            print("URL provided is not valid", url)
+            exit(1)
 
-    if not is_valid_url(url):
-        print("URL provided is not valid", url)
-        exit(1)
+        urls_to_read.append(url)
 
     output_directory_name = "output"
 
     os.makedirs("output", exist_ok=True)
 
-    feed_results = load_feed(url)
+    for url in urls_to_read:
+        feed_results = load_feed(url)
 
-    for item in feed_results:
-        # print(item)
-        filename, content = convert_content(item["url"])
+        for item in feed_results:
+            # print(item)
+            filename, content = convert_content(item["url"])
 
-        output_path = os.path.join(output_directory_name, filename)
+            output_path = os.path.join(output_directory_name, filename)
 
-        with open(output_path, "w") as output_file:
-            output_file.write(content)
+            if not output_path.exists():
+                with open(output_path, "w") as output_file:
+                    output_file.write(content)
