7c3bdf0f85b01c89dcfc699b4871c24551eb8c8b
Robert Rees <shudderfix@gmail.com>
2025-04-05 11:09:32 +0100

    Adjusts some of the information and calculates to make them a bit more helpful

--- interest/overpayment.py
+++ interest/overpayment.py
@@ -14,6 +14,16 @@ interest_rate = float(sys.argv[2])
 adjustment = float(sys.argv[3])
 
 
+def calculate_interest(principle, interest_rate):
+    monthly_interest_rate = interest_rate / 12
+
+    monthly_interest = (principle * monthly_interest_rate) / 100
+
+    daily_interest = monthly_interest / 30
+
+    return monthly_interest, daily_interest
+
+
 def calculate_daily_interest(principle, interest_rate):
     total_interest = (principle * interest_rate) / 100
 
@@ -43,14 +53,34 @@ def display_projection(principle, interest_rate, adjustment, days=30):
     base_projection = interest_projection(principle, interest_rate)
     adjusted_projection = interest_projection(principle + adjustment, interest_rate)
 
-    for [[base_balance, base_interest], [adjusted_balance, adjusted_interest]] in zip(
-        islice(base_projection, days), islice(adjusted_projection, days)
+    for [
+        day,
+        [[base_balance, base_interest], [adjusted_balance, adjusted_interest]],
+    ] in enumerate(
+        zip(islice(base_projection, days), islice(adjusted_projection, days))
     ):
         balance_difference = adjusted_balance - base_balance - adjustment
-        interest_difference = adjusted_interest - base_interest
+
         print(
-            f"Balance: {adjusted_balance:.2f} ({balance_difference:.2f}), daily interest {adjusted_interest:.2f} ({interest_difference:.2f})"
+            f"Day {day + 1}: balance: {adjusted_balance:.2f} ({balance_difference:.2f}); daily interest {calculate_daily_interest(adjusted_balance, interest_rate):.2f}"
         )
 
+
+[initial_monthly_interest, initial_daily_interest] = calculate_interest(
+    principle, interest_rate
+)
+print(
+    f"Monthly interest: {initial_monthly_interest:.2f}; daily interest {initial_daily_interest:.2f}"
+)
+
+[adjusted_monthly_interest, adjusted_daily_interest] = calculate_interest(
+    principle + adjustment, interest_rate
+)
+
+print("After adjustment:")
+print(
+    f"Monthly interest: {adjusted_monthly_interest:.2f}; daily interest {adjusted_daily_interest:.2f}"
+)
+print()
 
 display_projection(principle, interest_rate, adjustment)
